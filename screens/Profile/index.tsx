9efc0d0d90f21cf625bf8e4e28d7cdb289e21444
Markus Lund <markuslund92@gmail.com>
2022-03-09 16:32:15 +0100

    Add datepicker to profile screen

--- screens/Profile/index.tsx
+++ screens/Profile/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
 import {dateToNorwegianString} from '../../helpers';
 import {ProfileScreenProps} from '../../types';
 
 const ProfileScreen = ({route}: ProfileScreenProps) => {
-  const [newGullkorn, setNewGullkorn] = React.useState('');
-  const [gullkornDate, setGullkornDate] = React.useState(new Date());
+  const [newGullkorn, setNewGullkorn] = useState('');
+  const [gullkornDate, setGullkornDate] = useState(new Date());
+
+  const [datePickerOpen, setDatePickerOpen] = useState(false);
 
   return (
     <SafeAreaView style={styles.profileScreen}>
@@ -19,32 +30,77 @@ const ProfileScreen = ({route}: ProfileScreenProps) => {
         onChangeText={text => setNewGullkorn(text)}
         value={newGullkorn}
       />
+      <View style={styles.buttonRow}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setDatePickerOpen(true)}>
+          <Text>Velg dato</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            Alert.alert('🏆🌽', '', [
+              {
+                text: '⭐️',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+              },
+            ])
+          }>
+          <Text>Lagre gullkorn</Text>
+        </TouchableOpacity>
+      </View>
+      <DatePicker
+        modal
+        open={datePickerOpen}
+        date={gullkornDate}
+        onConfirm={date => {
+          setDatePickerOpen(false);
+          setGullkornDate(date);
+        }}
+        onCancel={() => {
+          setDatePickerOpen(false);
+        }}
+        mode="date"
+        locale="nb-NO"
+        confirmText="Bekreft"
+        cancelText="Avbryt"
+        title="Velg dato"
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    margin: 10,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
   profileScreen: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
   },
   gullkornText: {
-    // width: '90%',
     fontSize: 20,
     fontStyle: 'italic',
     height: 40,
     borderBottomWidth: 1,
     padding: 10,
+    color: '#333',
   },
   gullkornDate: {
-    // width: '90%',
     fontSize: 20,
     height: 40,
     borderBottomWidth: 1,
     padding: 10,
   },
   gullkornAuthor: {
-    // width: '90%',
     fontSize: 20,
     fontStyle: 'italic',
     height: 40,
