5ed7a81688ac1a2401ad688c53ba65afc2b9c101
Markus Lund <markuslund92@gmail.com>
2022-03-09 14:14:54 +0100

    Navigation with types

--- App.tsx
+++ App.tsx
@@ -1,13 +1,65 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 
-const App = () => {
+type RootStackParamList = {
+  Profile: {personName: string};
+  Home: undefined;
+};
+
+const profileScreenName = 'Profile';
+type ProfileScreenProps = NativeStackScreenProps<RootStackParamList, 'Profile'>;
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const names = ['Sondre', 'Ivar', 'Mathias'];
+
+const HomeScreen = ({navigation}: HomeScreenProps) => {
+  return (
+    <SafeAreaView style={styles.screen}>
+      {names.map(name => (
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() =>
+            navigation.navigate(profileScreenName, {personName: name})
+          }>
+          <Text>{name}</Text>
+        </TouchableOpacity>
+      ))}
+    </SafeAreaView>
+  );
+};
+
+const ProfileScreen = ({route}: ProfileScreenProps) => {
   return (
     <SafeAreaView style={styles.screen}>
       <TouchableOpacity style={styles.roundButton}>
-        <Text>Hei, Appgarasjen!</Text>
+        <Text>{route.params.personName}</Text>
       </TouchableOpacity>
     </SafeAreaView>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Gullkorn'}}
+        />
+        <Stack.Screen
+          name={profileScreenName}
+          component={ProfileScreen}
+          options={({route}) => ({title: route.params.personName})}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
